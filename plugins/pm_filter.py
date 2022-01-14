1c565a58320d7552367520e05ec74737e6ba698b
REX BOTZ <89206214+REX-BOTZ@users.noreply.github.com>
2022-01-14 15:28:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -469,26 +469,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
-    elif query.data == "manuelfilter":
-        buttons = [[
-            InlineKeyboardButton(' 🔙 ʙᴀᴄᴋ', callback_data='help')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
-            text=script.MANUELFILTER_TXT,
-            reply_markup=reply_markup,
-            parse_mode='html'
-        )
-    elif query.data == "autofilter":
-        buttons = [[
-            InlineKeyboardButton('🔙 ʙᴀᴄᴋ', callback_data='help')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
-            text=script.AUTOFILTER_TXT,
-            reply_markup=reply_markup,
-            parse_mode='html'
-        )
     elif query.data == "about":
         buttons= [[
             InlineKeyboardButton('👤 ᴏᴛʜᴇʀ ʙᴏᴛs',url='https://t.me/REX_BOTZ/94')
