e8d6a0211c961d8465f32aa4f4e19a936b21447f
REX BOTZ <89206214+REX-BOTZ@users.noreply.github.com>
2022-01-16 14:23:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -441,7 +441,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.FSTORE_XT,
+            text=script.FSTORE_TXT,
             reply_markup=reply_markup,
             parse_mode='html'
         )
