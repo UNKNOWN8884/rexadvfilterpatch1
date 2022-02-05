24b6027cb376a7e3236ffa1e9afaba9fe2d6d545
Tanujairam <88611197+Tanujairam123@users.noreply.github.com>
2022-02-05 20:02:06 +0530

    Update purge.py

--- plugins/admin/purge.py
+++ plugins/admin/purge.py
@@ -7,7 +7,7 @@ from info import (
     COMMAND_HAND_LER,
     TG_MAX_SELECT_LEN
 )
-from plugins.helper_functions.admin_check import admin_check
+from plugins.helper_functions.admin_checks import admin_check
 from plugins.helper_functions.cust_p_filters import f_onw_fliter
 
 
