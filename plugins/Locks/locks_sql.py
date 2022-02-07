68c1da5e25b005b3ea1329daaddce310ae770a74
Tanujairam <88611197+Tanujairam123@users.noreply.github.com>
2022-02-07 20:33:26 +0530

    Update locks_sql.py

--- plugins/Locks/locks_sql.py
+++ plugins/Locks/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from plugins.Locks.modules import SESSION, BASE
+from plugins.Locks.modules.__init__ import SESSION, BASE
 
 
 class Permissions(BASE):
