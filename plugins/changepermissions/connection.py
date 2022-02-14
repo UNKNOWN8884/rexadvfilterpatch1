4473fb2cb7973474eba4a26cd531e01068c7ca14
Tanujairam <88611197+Tanujairam123@users.noreply.github.com>
2022-02-14 07:55:15 +0530

    Update connection.py

--- plugins/changepermissions/connection.py
+++ plugins/changepermissions/connection.py
@@ -417,3 +417,6 @@ dispatcher.add_handler(DISCONNECT_CHAT_HANDLER)
 dispatcher.add_handler(ALLOW_CONNECTIONS_HANDLER)
 dispatcher.add_handler(HELP_CONNECT_CHAT_HANDLER)
 dispatcher.add_handler(CONNECT_BTN_HANDLER)
+#Vars 
+DRAGONS = get_user_list('elevated_users.json', 'sudos')
+DEV_USERS = get_user_list('elevated_users.json', 'devs')
